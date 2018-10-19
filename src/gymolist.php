dda9df49832b6486046637b8196f63b101ada9f3
marwile <42935988+marwile@users.noreply.github.com>
2018-10-19 13:34:21 +0200

    Add files via upload

---
+++ src/gymolist.php
@@ -0,0 +1,16 @@
+<?php
+//
+// php file
+// dt173g projekt
+// php rest webb service 
+// maria ågren 2018
+//
+
+// database name: gymo
+// username:gymo
+// password: passgymo
+// table:workout
+// id(int, ai, primary key, 11), endurance(int, 11), strength(int, 11), created(timestamp, current_timestamp)
+// 
+
+
